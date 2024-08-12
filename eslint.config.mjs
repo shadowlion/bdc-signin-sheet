650a4f9e2a745a820da9e178fff1e3e7fa3fe370
Justin Chiou <chiou.kai@gmail.com>
2024-08-12 18:07:12 -0400

    refax: use vite/vue instead

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,11 @@
 import antfu from "@antfu/eslint-config";
-import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  antfu({
-    typescript: true,
-    vue: true,
-    stylistic: {
-      indent: 2,
-      quotes: "double",
-      semi: true,
-    },
-  }),
-);
+export default antfu({
+  vue: true,
+  typescript: true,
+  stylistic: {
+    semi: true,
+    quotes: "double",
+    indent: 2,
+  },
+});
