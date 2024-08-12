3693d5ae44d05fa983d1aa4915af6f197187a0c3
Justin Chiou <chiou.kai@gmail.com>
2024-08-12 18:10:31 -0400

    refax: two section division

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import SectionForm from "./components/SectionForm.vue";
+import SectionHistory from "./components/SectionHistory.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo">
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <fieldset>
+    <legend>Form</legend>
+    <SectionForm />
+  </fieldset>
+  <fieldset>
+    <legend>History</legend>
+    <SectionHistory />
+  </fieldset>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/SectionHistory.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <h1>History Section</h1>
+  </section>
+</template>
