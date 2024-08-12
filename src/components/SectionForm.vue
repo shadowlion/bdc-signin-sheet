2cf042a175b1223350fc7414b9eeb4bf7c28e74e
Justin Chiou <chiou.kai@gmail.com>
2024-08-12 18:35:10 -0400

    feat: basic form

--- src/components/SectionForm.vue
+++ src/components/SectionForm.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const formData = ref({
+  name: "",
+  instructor: "",
+  dateStart: "",
+  dateEnd: "",
+});
+
+function handleFormSubmit() {
+  // eslint-disable-next-line no-console
+  console.log(JSON.stringify(formData.value));
+}
+</script>
+
 <template>
   <section>
-    <h1>Form Section</h1>
+    <form @submit.prevent="handleFormSubmit()">
+      <input id="name" v-model="formData.name" type="text" name="name">
+      <input id="instructor" v-model="formData.instructor" type="text" name="instructor">
+      <input id="date-start" v-model="formData.dateStart" type="time" name="dateStart">
+      <input id="date-end" v-model="formData.dateEnd" type="time" name="dateEnd">
+      <button type="submit">
+        Submit
+      </button>
+    </form>
   </section>
 </template>
