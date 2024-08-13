bdab5415ee2cc314a0a9fcde46fd3b62d163f6e7
Justin Chiou <chiou.kai@gmail.com>
2024-08-13 17:29:12 -0400

    feat: Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import "@picocss/pico/css/pico.min.css";
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 
 const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
 app.mount("#app");
