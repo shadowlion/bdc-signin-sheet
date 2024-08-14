598459167ad19942c38e5b07400a6ce904c03c44
Justin Chiou <chiou.kai@gmail.com>
2024-08-13 20:51:06 -0400

    fix: grid layout

--- src/components/SectionForm.vue
+++ src/components/SectionForm.vue
@@ -13,10 +13,16 @@ const { handleFormSubmit } = useFormStore();
       <form @submit.prevent="handleFormSubmit()">
         <label for="names">Who is Practicing?</label>
         <input id="names" v-model="formData.names" type="text" aria-label="Names" placeholder="John and Jane Doe">
-        <label for="date-start">Start Time</label>
-        <input id="date-start" v-model="formData.datetimeStart" type="datetime-local" aria-label="Start Time">
-        <label for="date-end">End Time</label>
-        <input id="date-end" v-model="formData.datetimeEnd" type="datetime-local" aria-label="End Time">
+        <div class="grid">
+          <div>
+            <label for="date-start">Start Time</label>
+            <input id="date-start" v-model="formData.datetimeStart" type="datetime-local" aria-label="Start Time">
+          </div>
+          <div>
+            <label for="date-end">End Time</label>
+            <input id="date-end" v-model="formData.datetimeEnd" type="datetime-local" aria-label="End Time">
+          </div>
+        </div>
         <button v-if="submitting" aria-busy="true" aria-label="Submitting..." type="submit" disabled>
           Submitting...
         </button>
